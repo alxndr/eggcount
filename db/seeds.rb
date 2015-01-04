2abed80d91574fbbecf31e7bfa6cd906871fc2ce
Alexander <alxndr+github@gmail.com>
2015-01-04 15:14:08 -0800

    WIP example seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  { date: Date.new(2014, 10, 10), count: 0 },
+].each do |new_day_attributes|
+  Day.create new_day_attributes
+end
