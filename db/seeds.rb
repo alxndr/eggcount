42bb2cdc7eafe7fc4d9cbafd8abbe0c03ac77497
Alexander <alxndr+github@gmail.com>
2015-01-04 19:16:00 -0800

    seed data for 2014

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 [
   { date: Date.new(2014,  1, 20), count: 5 },
+
   { date: Date.new(2014,  1, 22), count: 3 },
   { date: Date.new(2014,  1, 23), count: 2 },
+
+
   { date: Date.new(2014,  1, 26), count: 5 },
   { date: Date.new(2014,  1, 27), count: 1 },
   { date: Date.new(2014,  1, 28), count: 1 },
@@ -13,8 +16,334 @@
   { date: Date.new(2014,  2,  3), count: 1 },
   { date: Date.new(2014,  2,  4), count: 3 },
   { date: Date.new(2014,  2,  5), count: 3 },
+
   { date: Date.new(2014,  2,  7), count: 5 },
   { date: Date.new(2014,  2,  8), count: 8 },
+  { date: Date.new(2014,  2,  9), count: 7 },
+
+  { date: Date.new(2014,  2, 11), count: 6 },
+  { date: Date.new(2014,  2, 12), count: 2 },
+  { date: Date.new(2014,  2, 13), count: 7 },
+  { date: Date.new(2014,  2, 14), count: 2 },
+  { date: Date.new(2014,  2, 15), count: 6 },
+  { date: Date.new(2014,  2, 16), count: 7 },
+
+
+  { date: Date.new(2014,  2, 19), count: 2 },
+  { date: Date.new(2014,  2, 20), count: 7 },
+  { date: Date.new(2014,  2, 21), count: 7 },
+  { date: Date.new(2014,  2, 22), count: 3 },
+  { date: Date.new(2014,  2, 23), count: 2 },
+  { date: Date.new(2014,  2, 24), count: 5 },
+  { date: Date.new(2014,  2, 25), count: 8 },
+  { date: Date.new(2014,  2, 26), count: 0 },
+  { date: Date.new(2014,  2, 27), count: 3 },
+  { date: Date.new(2014,  2, 28), count: 3 },
+  { date: Date.new(2014,  3,  1), count: 8 },
+
+  { date: Date.new(2014,  3,  3), count: 8 },
+  { date: Date.new(2014,  3,  4), count: 3 },
+
+  { date: Date.new(2014,  3,  6), count: 10 },
+  { date: Date.new(2014,  3,  7), count: 5 },
+  { date: Date.new(2014,  3,  8), count: 4 },
+  { date: Date.new(2014,  3,  9), count: 6 },
+  { date: Date.new(2014,  3, 10), count: 3 },
+  { date: Date.new(2014,  3, 11), count: 7 },
+  { date: Date.new(2014,  3, 12), count: 1 },
+  { date: Date.new(2014,  3, 13), count: 4 },
+  { date: Date.new(2014,  3, 14), count: 9 },
+  { date: Date.new(2014,  3, 15), count: 2 },
+
+  { date: Date.new(2014,  3, 17), count: 7 },
+  { date: Date.new(2014,  3, 18), count: 1 },
+
+  { date: Date.new(2014,  3, 20), count: 3 },
+  { date: Date.new(2014,  3, 21), count: 4 },
+  { date: Date.new(2014,  3, 22), count: 6 },
+  { date: Date.new(2014,  3, 23), count: 3 },
+  { date: Date.new(2014,  3, 24), count: 2 },
+  { date: Date.new(2014,  3, 25), count: 3 },
+  { date: Date.new(2014,  3, 26), count: 7 },
+
+  { date: Date.new(2014,  3, 28), count: 3 },
+  { date: Date.new(2014,  3, 29), count: 12 },
+  { date: Date.new(2014,  3, 30), count: 3 },
+
+  { date: Date.new(2014,  4,  1), count: 7 },
+  { date: Date.new(2014,  4,  2), count: 5 },
+  { date: Date.new(2014,  4,  3), count: 4 },
+  { date: Date.new(2014,  4,  4), count: 3 },
+  { date: Date.new(2014,  4,  5), count: 2 },
+  { date: Date.new(2014,  4,  6), count: 12 },
+  { date: Date.new(2014,  4,  7), count: 6 },
+  { date: Date.new(2014,  4,  8), count: 4 },
+
+  { date: Date.new(2014,  4, 10), count: 5 },
+  { date: Date.new(2014,  4, 11), count: 1 },
+  { date: Date.new(2014,  4, 12), count: 1 },
+  { date: Date.new(2014,  4, 13), count: 7 },
+  { date: Date.new(2014,  4, 14), count: 1 },
+  { date: Date.new(2014,  4, 15), count: 7 },
+  { date: Date.new(2014,  4, 16), count: 4 },
+  { date: Date.new(2014,  4, 17), count: 3 },
+  { date: Date.new(2014,  4, 18), count: 5 },
+  { date: Date.new(2014,  4, 19), count: 5 },
+  { date: Date.new(2014,  4, 20), count: 4 },
+  { date: Date.new(2014,  4, 21), count: 1 },
+  { date: Date.new(2014,  4, 22), count: 5 },
+  { date: Date.new(2014,  4, 23), count: 5 },
+
+  { date: Date.new(2014,  4, 25), count: 7 },
+  { date: Date.new(2014,  4, 26), count: 5 },
+  { date: Date.new(2014,  4, 27), count: 6 },
+  { date: Date.new(2014,  4, 28), count: 3 },
+  { date: Date.new(2014,  4, 29), count: 3 },
+
+  { date: Date.new(2014,  5,  1), count: 13 },
+  { date: Date.new(2014,  5,  2), count: 2 },
+  { date: Date.new(2014,  5,  3), count: 7 },
+  { date: Date.new(2014,  5,  4), count: 3 },
+  { date: Date.new(2014,  5,  5), count: 8 },
+  { date: Date.new(2014,  5,  6), count: 6 },
+  { date: Date.new(2014,  5,  7), count: 3 },
+  { date: Date.new(2014,  5,  8), count: 8 },
+  { date: Date.new(2014,  5,  9), count: 1 },
+  { date: Date.new(2014,  5, 10), count: 7 },
+  { date: Date.new(2014,  5, 11), count: 4 },
+  { date: Date.new(2014,  5, 12), count: 3 },
+
+
+  { date: Date.new(2014,  5, 15), count: 8 },
+  { date: Date.new(2014,  5, 16), count: 1 },
+  { date: Date.new(2014,  5, 17), count: 5 },
+  { date: Date.new(2014,  5, 18), count: 5 },
+  { date: Date.new(2014,  5, 19), count: 8 },
+  { date: Date.new(2014,  5, 20), count: 2 },
+
+  { date: Date.new(2014,  5, 22), count: 10 },
+
+  { date: Date.new(2014,  5, 24), count: 7 },
+  { date: Date.new(2014,  5, 25), count: 7 },
+
+  { date: Date.new(2014,  5, 27), count: 6 },
+
+  { date: Date.new(2014,  5, 29), count: 9 },
+  { date: Date.new(2014,  5, 30), count: 3 },
+  { date: Date.new(2014,  5, 31), count: 5 },
+
+  { date: Date.new(2014,  6,  2), count: 7 },
+  { date: Date.new(2014,  6,  3), count: 2 },
+
+  { date: Date.new(2014,  6,  5), count: 3 },
+  { date: Date.new(2014,  6,  6), count: 2 },
+  { date: Date.new(2014,  6,  7), count: 5 },
+  { date: Date.new(2014,  6,  8), count: 6 },
+
+  { date: Date.new(2014,  6, 10), count: 5 },
+  { date: Date.new(2014,  6, 11), count: 3 },
+  { date: Date.new(2014,  6, 12), count: 5 },
+  { date: Date.new(2014,  6, 13), count: 6 },
+  { date: Date.new(2014,  6, 14), count: 4 },
+  { date: Date.new(2014,  6, 15), count: 2 },
+  { date: Date.new(2014,  6, 16), count: 10 },
+  { date: Date.new(2014,  6, 17), count: 5 },
+  { date: Date.new(2014,  6, 18), count: 4 },
+  { date: Date.new(2014,  6, 19), count: 5 },
+  { date: Date.new(2014,  6, 20), count: 4 },
+
+
+  { date: Date.new(2014,  6, 23), count: 15 },
+  { date: Date.new(2014,  6, 24), count: 8 },
+  { date: Date.new(2014,  6, 25), count: 4 },
+
+  { date: Date.new(2014,  6, 27), count: 10 },
+  { date: Date.new(2014,  6, 28), count: 5 },
+  { date: Date.new(2014,  6, 29), count: 6 },
+  { date: Date.new(2014,  6, 30), count: 3 },
+  { date: Date.new(2014,  7,  1), count: 5 },
+  { date: Date.new(2014,  7,  2), count: 4 },
+  { date: Date.new(2014,  7,  3), count: 4 },
+  { date: Date.new(2014,  7,  4), count: 3 },
+  { date: Date.new(2014,  7,  5), count: 3 },
+
+  { date: Date.new(2014,  7,  7), count: 4 },
+
+  { date: Date.new(2014,  7,  9), count: 5 },
+  { date: Date.new(2014,  7, 10), count: 5 },
+  { date: Date.new(2014,  7, 11), count: 9 },
+  { date: Date.new(2014,  7, 12), count: 1 },
+
+  { date: Date.new(2014,  7, 14), count: 1 },
+  { date: Date.new(2014,  7, 15), count: 2 },
+  { date: Date.new(2014,  7, 16), count: 8 },
+  { date: Date.new(2014,  7, 17), count: 4 },
+
+  { date: Date.new(2014,  7, 19), count: 8 },
+
+  { date: Date.new(2014,  7, 21), count: 6 },
+
+  { date: Date.new(2014,  7, 23), count: 5 },
+  { date: Date.new(2014,  7, 24), count: 8 },
+
+  { date: Date.new(2014,  7, 26), count: 5 },
+  { date: Date.new(2014,  7, 27), count: 7 },
+  { date: Date.new(2014,  7, 28), count: 1 },
+
+
+  { date: Date.new(2014,  7, 31), count: 3 },
+  { date: Date.new(2014,  8,  1), count: 9 },
+  { date: Date.new(2014,  8,  2), count: 4 },
+
+
+  { date: Date.new(2014,  8,  5), count: 2 },
+  { date: Date.new(2014,  8,  6), count: 4 },
+  { date: Date.new(2014,  8,  7), count: 4 },
+  { date: Date.new(2014,  8,  8), count: 3 },
+  { date: Date.new(2014,  8,  9), count: 4 },
+  { date: Date.new(2014,  8, 10), count: 2 },
+  { date: Date.new(2014,  8, 11), count: 1 },
+
+  { date: Date.new(2014,  8, 13), count: 5 },
+  { date: Date.new(2014,  8, 14), count: 7 },
+
+  { date: Date.new(2014,  8, 16), count: 12 },
+  { date: Date.new(2014,  8, 17), count: 4 },
+  { date: Date.new(2014,  8, 18), count: 5 },
+  { date: Date.new(2014,  8, 19), count: 3 },
+
+
+  { date: Date.new(2014,  8, 22), count: 8 },
+  { date: Date.new(2014,  8, 23), count: 5 },
+  { date: Date.new(2014,  8, 24), count: 4 },
+  { date: Date.new(2014,  8, 25), count: 2 },
+  { date: Date.new(2014,  8, 26), count: 4 },
+  { date: Date.new(2014,  8, 27), count: 3 },
+  { date: Date.new(2014,  8, 28), count: 7 },
+
+  { date: Date.new(2014,  8, 30), count: 5 },
+  { date: Date.new(2014,  8, 31), count: 7 },
+  { date: Date.new(2014,  9,  1), count: 5 },
+  { date: Date.new(2014,  9,  2), count: 4 },
+  { date: Date.new(2014,  9,  3), count: 4 },
+  { date: Date.new(2014,  9,  4), count: 5 },
+  { date: Date.new(2014,  9,  5), count: 3 },
+  { date: Date.new(2014,  9,  6), count: 8 },
+  { date: Date.new(2014,  9,  7), count: 7 },
+  { date: Date.new(2014,  9,  8), count: 3 },
+  { date: Date.new(2014,  9,  9), count: 5 },
+  { date: Date.new(2014,  9, 10), count: 3 },
+  { date: Date.new(2014,  9, 11), count: 4 },
+
+  { date: Date.new(2014,  9, 13), count: 8 },
+  { date: Date.new(2014,  9, 14), count: 1 },
+  { date: Date.new(2014,  9, 15), count: 5 },
+  { date: Date.new(2014,  9, 16), count: 3 },
+  { date: Date.new(2014,  9, 17), count: 5 },
+
+  { date: Date.new(2014,  9, 19), count: 9 },
+  { date: Date.new(2014,  9, 20), count: 1 },
+  { date: Date.new(2014,  9, 21), count: 9 },
+  { date: Date.new(2014,  9, 22), count: 3 },
+  { date: Date.new(2014,  9, 23), count: 3 },
+  { date: Date.new(2014,  9, 24), count: 5 },
+
+  { date: Date.new(2014,  9, 26), count: 3 },
+  { date: Date.new(2014,  9, 27), count: 7 },
+  { date: Date.new(2014,  9, 28), count: 4 },
+  { date: Date.new(2014,  9, 29), count: 2 },
+  { date: Date.new(2014,  9, 30), count: 5 },
+  { date: Date.new(2014, 10,  1), count: 4 },
+  { date: Date.new(2014, 10,  2), count: 2 },
+
+  { date: Date.new(2014, 10,  4), count: 7 },
+  { date: Date.new(2014, 10,  5), count: 6 },
+  { date: Date.new(2014, 10,  6), count: 1 },
+  { date: Date.new(2014, 10,  7), count: 5 },
+  { date: Date.new(2014, 10,  8), count: 2 },
+  { date: Date.new(2014, 10,  9), count: 6 },
+
+  { date: Date.new(2014, 10, 11), count: 6 },
+  { date: Date.new(2014, 10, 12), count: 2 },
+  { date: Date.new(2014, 10, 13), count: 4 },
+  { date: Date.new(2014, 10, 14), count: 4 },
+
+  { date: Date.new(2014, 10, 16), count: 5 },
+
+
+  { date: Date.new(2014, 10, 19), count: 4 },
+  { date: Date.new(2014, 10, 20), count: 7 },
+
+  { date: Date.new(2014, 10, 22), count: 8 },
+  { date: Date.new(2014, 10, 23), count: 7 },
+  { date: Date.new(2014, 10, 24), count: 5 },
+
+  { date: Date.new(2014, 10, 26), count: 6 },
+
+
+
+  { date: Date.new(2014, 10, 30), count: 14 },
+
+  { date: Date.new(2014, 11,  1), count: 4 },
+  { date: Date.new(2014, 11,  2), count: 4 },
+  { date: Date.new(2014, 11,  3), count: 4 },
+  { date: Date.new(2014, 11,  4), count: 1 },
+  { date: Date.new(2014, 11,  5), count: 3 },
+
+  { date: Date.new(2014, 11,  7), count: 5 },
+  { date: Date.new(2014, 11,  8), count: 4 },
+  { date: Date.new(2014, 11,  9), count: 1 },
+  { date: Date.new(2014, 11, 10), count: 3 },
+  { date: Date.new(2014, 11, 11), count: 1 },
+  { date: Date.new(2014, 11, 12), count: 4 },
+  { date: Date.new(2014, 11, 13), count: 4 },
+  { date: Date.new(2014, 11, 14), count: 2 },
+  { date: Date.new(2014, 11, 15), count: 2 },
+  { date: Date.new(2014, 11, 16), count: 3 },
+  { date: Date.new(2014, 11, 17), count: 2 },
+  { date: Date.new(2014, 11, 18), count: 2 },
+  { date: Date.new(2014, 11, 19), count: 3 },
+
+  { date: Date.new(2014, 11, 21), count: 5 },
+
+  { date: Date.new(2014, 11, 23), count: 3 },
+  { date: Date.new(2014, 11, 24), count: 1 },
+  { date: Date.new(2014, 11, 25), count: 2 },
+  { date: Date.new(2014, 11, 26), count: 3 },
+  { date: Date.new(2014, 11, 27), count: 3 },
+  { date: Date.new(2014, 11, 28), count: 3 },
+
+  { date: Date.new(2014, 11, 30), count: 3 },
+  { date: Date.new(2014, 12,  1), count: 3 },
+
+  { date: Date.new(2014, 12,  3), count: 1 },
+  { date: Date.new(2014, 12,  4), count: 7 },
+
+  { date: Date.new(2014, 12,  6), count: 4 },
+  { date: Date.new(2014, 12,  7), count: 3 },
+
+  { date: Date.new(2014, 12,  9), count: 6 },
+
+
+  { date: Date.new(2014, 12, 12), count: 4 },
+  { date: Date.new(2014, 12, 13), count: 3 },
+  { date: Date.new(2014, 12, 14), count: 2 },
+
+
+
+  { date: Date.new(2014, 12, 18), count: 8 },
+  { date: Date.new(2014, 12, 19), count: 3 },
+  { date: Date.new(2014, 12, 21), count: 3 },
+  { date: Date.new(2014, 12, 22), count: 2 },
+  { date: Date.new(2014, 12, 23), count: 2 },
+  { date: Date.new(2014, 12, 24), count: 2 },
+  { date: Date.new(2014, 12, 25), count: 2 },
+
+
+
+
+  { date: Date.new(2014, 12, 30), count: 1 },
+
 ].each do |new_day_attributes|
   Day.create new_day_attributes
 end
