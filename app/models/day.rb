d19c1d31d0d64810d98ab87d932c1a3c4de652a1
Alexander <alxndr+github@gmail.com>
2015-01-05 00:26:23 -0800

    calculate moving averages
    * ...in a very inefficient way
    * tweak charts too

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,8 +3,12 @@ class Day < ActiveRecord::Base
   validates :count, presence: true
   validates :date, presence: true, uniqueness: true
 
-  def calculate_moving_average_days(n) # this *could* be done in sql...
-    Day.where(date: (date - n.days)..(date - 1.minute)).order(:date).pluck(:count).sum / n.to_f
+  def calculate_moving_average_days(n) # this should be done in sql
+    range_start = (date - n.days)
+    return nil unless Day.where("date < ?", range_start).present?
+    range_end = date - 1.minute
+    days_in_range = Day.where(date: range_start..range_end)
+    days_in_range.order(:date).pluck(:count).sum / n.to_f
   end
 
 end
--- app/views/api/years/index.json.jbuilder
+++ app/views/api/years/index.json.jbuilder
@@ -3,10 +3,9 @@ json.years @years do |year|
   json.days year[:days] do |day|
     json.date day.date
     json.count day.count
-    json.moving_averages do
-      json.day7 day.calculate_moving_average_days(7)
-      json.day14 day.calculate_moving_average_days(14)
-      json.day28 day.calculate_moving_average_days(28)
-    end
+    json.avg7 day.calculate_moving_average_days(7)
+    #json.avg14 day.calculate_moving_average_days(14)
+    json.avg28 day.calculate_moving_average_days(28)
+    json.avg84 day.calculate_moving_average_days(84)
   end
 end
