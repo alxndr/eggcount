f1c0138a958551a85e9f4884142e43401571fcba
Alexander <alxndr+github@gmail.com>
2016-01-09 10:22:39 -0800

    capitalize things

--- test/controllers/days_controller_test.rb
+++ test/controllers/days_controller_test.rb
@@ -6,7 +6,7 @@ class DaysControllerTest < ActionController::TestCase
     Day.delete_all
   end
 
-  test 'should get index' do
+  test 'should GET index' do
     Day.create date: Date.today, count: 2
 
     get :index
@@ -15,7 +15,7 @@ class DaysControllerTest < ActionController::TestCase
     assert_not_nil assigns(:days)
   end
 
-  test 'should post create' do
+  test 'should POST create' do
     assert_difference('Day.count') do
       post :create, day: { date: Date.parse('2014-06-23'), count: 11 }
     end
