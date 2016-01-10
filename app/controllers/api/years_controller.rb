4cff8b141d68eda2cd5202480ec1135d46150da8
Alexander <alxndr+github@gmail.com>
2016-01-09 23:39:29 -0800

    use rails for that controller

--- app/controllers/api/years_controller.rb
+++ app/controllers/api/years_controller.rb
@@ -1,14 +1,9 @@
 module Api
-  class YearsController < Api::BaseController
+  class YearsController < ApplicationController
 
     # GET /api/years.json
     def index
-      plural_resource_name = '@years'
-      resources = Year.find_stuff
-
-      instance_variable_set(plural_resource_name, resources)
-      expires_in 5.minutes, public: true
-      respond_with instance_variable_get(plural_resource_name)
+      @years = Year.find_stuff
     end
 
   end
