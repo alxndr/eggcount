29503815bed539b5b1ce263fc4c5bdd1c60b7930
Alexander <alxndr+github@gmail.com>
2016-05-22 11:42:06 -0700

    showing a chart

--- main.js
+++ main.js
@@ -2,188 +2,109 @@
 
 (function(context) {
 
-  // Object.prototype.tap = function(cb) {
-  //   cb(this);
-  //   return this;
-  // };
-
-  function objectValues(obj) {
-    return Object.keys(obj).map((key) => obj[key]);
-  }
-
-  function objectKeyValPairs(obj) {
-    return Object.keys(obj).map((key) => [key, obj[key]]);
-  }
-
-  function randomIntLessThan(max) {
-    return Math.floor(max * Math.random());
-  }
-
-  function isDayWithinNDaysBefore(numDays, beforeDate) {
-    let nDaysBeforeDate = new Date(beforeDate);
-    nDaysBeforeDate.setDate(nDaysBeforeDate.getDate() - numDays);
-    return function(dayInQuestion) {
-      const dateInQuestion = new Date(dayInQuestion.date);
-      const r = dateInQuestion > nDaysBeforeDate; // TODO this doesn't account for time zones so may be off by a day
-      // if (randomIntLessThan(999) % 7 == 0) console.log(`is ${beforeDate} within ${numDays} of ${dayInQuestion.date}? ${r ? "yep" : "nope"}`);
-      return r;
-    };
-  }
-
-  function averageOfNDaysBefore(numDays, beforeAndIncludingDate, data) {
-    if (!data.length) return 0;
-    const sumOfNDaysBeforeDate = data
-            .filter(isDayWithinNDaysBefore(numDays, beforeAndIncludingDate))
-            .reduce((sum, dayData) => sum + dayData.count, 0); // TODO this does not account for gaps in data
-    return sumOfNDaysBeforeDate / numDays;
-  }
-
-  function flattener(a, b) {
-    return a.concat(b);
-  }
-
-  function makeDayObject(data, moreData) {
-    var date = data.date;
-    var count = data.count;
-    const mD = Object.keys(moreData)
-            .map((key) => moreData[key])
-            .map((obj) => obj.days)
-            .map((arrayOfEntries) => {
-              return arrayOfEntries.map((entry) => {
-                return {
-                  date: entry.date,
-                  count: entry.count,
-                };
-              });
-            })
-            .reduce(flattener, [])
-            .reverse()
-    ;
-    const r = {
-      date: date,
-      count: count,
-      avg7: averageOfNDaysBefore(7, date, mD),
-      // avg28: averageOfNDaysBefore(28, date, mD),
-      // avg84: averageOfNDaysBefore(84, date, mD)
-    };
-    // console.log("day object", r);
-    return r;
-  }
-
-  function chartOptions(config) {
-    return {
-      series: config.series,
-      title: { text: config.title },
-      alignTicks: false,
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: { // don't display the dummy year; it's part of data
-          month: '%b',
-          year: '%b',
-        },
-        title: { text: 'date' },
-      },
-      yAxis: {
-        title: { text: 'number of eggs' },
-        min: 0,
-        endOnTick: false,
-      },
-      tooltip: {
-        headerFormat: '<b>{series.name}</b><br>', // fake the year
-        pointFormat: '{point.x:%b}: {point.y:.0f}',
-      },
-      plotOptions: {
-        series: {
-          animation: false,
-        },
-      },
-    };
-  }
-
-  function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-      return response;
-    }
-    let error = new Error(response.statusText);
-    error.response = response;
-    throw error;
-  }
-
-  function runningAverageOverPriorDays({year, month, day}, numDays, dateEntries) {
-    const cutoffDate = new Date(year, month, day);
-    cutoffDate.setDate(referenceDate.getdate() - numDays);
+  function runningAverageOverPriorDays({year: startingYear, month: startingMonth, day: startingDay}, numDays, dateEntries) {
+    const referenceDate = new Date(startingYear, startingMonth-1, startingDay);
+    const cutoffDate = new Date(startingYear, startingMonth-1, startingDay);
+    cutoffDate.setDate(cutoffDate.getDate() - numDays);
     let dateInQuestion = cutoffDate;
-    while (dateInQuestion < cutoffDate) {
-      const dataForThatDay = dateEntries[year][month][day];
-      console.log("data for that day!", dataForThatDay);
-    /* const dateRange = ...;
-       dateRange
-       .map(extract the data for each date from dateEntries)
-       .reduce(sum it up) */
+    let dataToAverage = [];
+    while (dateInQuestion <= referenceDate) {
+      const year = (dateInQuestion.getYear() + 1900).toString();
+      const month = padZero((dateInQuestion.getMonth() + 1).toString());
+      const day = padZero((dateInQuestion.getDate()).toString());
+      if (dateEntries[year] && dateEntries[year][month]) {
+        const dayData = dateEntries[year][month][day];
+        if (dayData) {
+          dataToAverage.push(dayData.count);
+        }
+      }
+      dateInQuestion.setDate(dateInQuestion.getDate() + 1);
     }
+    return dataToAverage.reduce(sum) / numDays;
   }
 
-  function buildEntryDictionary(dateEntries, {date, count}) {
+  function buildEntryDictionary(entries, {date, count}) {
     const [year, month, day] = date.split("-");
-    if (!dateEntries[year]) {
-      dateEntries[year] = {};
+    if (!entries[year]) {
+      entries[year] = {};
     }
-    if (!dateEntries[year][month]) {
-      dateEntries[year][month] = {};
+    if (!entries[year][month]) {
+      entries[year][month] = {};
     }
-    dateEntries[year][month][day] = {
+    entries[year][month][day] = {
       count: count,
     };
-    dateEntries[year][month][day].runningAverages = {
-      days7: runningAverageOverPriorDays({year, month, day}, 7, dateEntries)
-    };
-    return dateEntries;
+    return entries;
   }
 
-  const keys = Object.keys.bind(Object);
-
-  function calculateAverages(data) {
-    return keys(data).map((year) => {
-    });
-    /* return objectKeyValPairs(data).reduce((yearsData, [year, yearData]) => {
-       return objectKeyValPairs(yearData).reduce((monthsData, [month, monthData]) => {
-       return objectKeyValPairs(monthData).reduce((daysData, [day, dayData]) => {
-       return dayData;
-       }, [])
-       }, []);
-       }, []); */
-  }
-
-  function thinger(acc1, [year, yearData]) {
-    /* console.log("initial reducer", year); */
-    const transformedYearData = objectKeyValPairs(yearData).reduce((acc2, [month, monthData]) => {
-      /* console.log("inside the month processor...", month, acc2); */
-      const something = objectKeyValPairs(monthData).reduce((acc3, [day, count]) => {
-        /* console.log("day processor...", `${year}-${month}-${day}`, count); */
-        acc3.countSeries.push(count);
-        acc3.dateSeries.push(new Date(year, month-1, day));
-        return acc3;
-      }, {dateSeries: [], countSeries: []});
-      /* console.log("parted out", something); */
-      return {
-        countSeries: acc2.countSeries.concat(something.countSeries),
-        dateSeries: acc2.dateSeries.concat(something.dateSeries),
-      };
-    }, {dateSeries: [], countSeries: []});
-    /* console.log(transformedYearData); */
-    return {
-      countSeries: acc1.countSeries.concat(transformedYearData.countSeries),
-      dateSeries: acc1.dateSeries.concat(transformedYearData.dateSeries),
+  function sortByFirstElement([a], [b]) {
+    if (a < b) {
+      return -1;
     }
+    if (b < a) {
+      return 1;
+    }
+    return 0;
   }
-  function convertData(data) {
-    const {dateSeries, countSeries} = objectKeyValPairs(data).reduce(thinger, {dateSeries: [], countSeries: []});
-    return [{
-      type: "scatter",
-      mode: "markers",
-      x: dateSeries,
-      y: countSeries
-    }];
+
+  function someETL(yearAcc, [year, yearData]) {
+    const transformedYearData = objectKeyValPairs(yearData)
+      .sort(sortByFirstElement)
+      .reduce((monthAcc, [month, monthData]) => {
+        const something =
+          objectKeyValPairs(monthData)
+          .sort(sortByFirstElement)
+          .reduce((dayAcc, [day, dayData]) => {
+            const date = new Date(year, month-1, day);
+            dayAcc.dateSeries.push(date);
+            dayAcc.rawCount.push(dayData.count);
+            dayAcc.avgDays7.push(dayData.runningAverages.days7);
+            dayAcc.avgDays28.push(dayData.runningAverages.days28);
+            dayAcc.avgDays84.push(dayData.runningAverages.days84);
+            return dayAcc;
+          }, newEmptyDataThing())
+        ;
+        return {
+          dateSeries: monthAcc.dateSeries.concat(something.dateSeries),
+          rawCount: monthAcc.rawCount.concat(something.rawCount),
+          avgDays7: monthAcc.avgDays7.concat(something.avgDays7),
+          avgDays28: monthAcc.avgDays28.concat(something.avgDays28),
+          avgDays84: monthAcc.avgDays84.concat(something.avgDays84),
+        };
+      }, newEmptyDataThing())
+    ;
+    return {
+      dateSeries: yearAcc.dateSeries.concat(transformedYearData.dateSeries),
+      rawCount: yearAcc.rawCount.concat(transformedYearData.rawCount),
+      avgDays7: yearAcc.avgDays7.concat(transformedYearData.avgDays7),
+      avgDays28: yearAcc.avgDays28.concat(transformedYearData.avgDays28),
+      avgDays84: yearAcc.avgDays84.concat(transformedYearData.avgDays84),
+    };
+  }
+
+  function newEmptyDataThing() {
+    return {
+      dateSeries: [],
+      rawCount: [],
+      avgDays7: [],
+      avgDays28: [],
+      avgDays84: [],
+    };
+  }
+
+  function calculateAverages(entryDictionary) {
+    for (const year in entryDictionary) {
+      for (const month in entryDictionary[year]) {
+        for (const day in entryDictionary[year][month]) {
+          entryDictionary[year][month][day].runningAverages = {
+            days7:  runningAverageOverPriorDays({year, month, day}, 7, entryDictionary),
+            days28: runningAverageOverPriorDays({year, month, day}, 28, entryDictionary),
+            days84: runningAverageOverPriorDays({year, month, day}, 84, entryDictionary),
+          };
+        }
+      }
+    }
+    return entryDictionary;
   }
 
   (function() {
@@ -193,12 +114,36 @@
       fetch('https://gist.githubusercontent.com/alxndr/c5cb1b4ceaf938d8801b60fd241fabf9/raw/1712731e092b6e94a350abb9a52e6a9b7b550fe2/eggcount.json')
         .then(checkStatus)
         .then((response) => response.json())
-        .then((data) => data.reduce(buildEntryDictionary, {})) // maybe this is a bad idea
-        .then((entryDictionary) => console.debug(entryDictionary) || entryDictionary)
-        .then((data) => calculateAverages(data))
+        .then((data) => data.reduce(buildEntryDictionary, {}))
+        .then((data) => calculateAverages(data)) // needs to be a separate pass
         .then((data) => {
-          const dataForPlotly = convertData(data);
-          console.log("ready??", dataForPlotly);
+          const allTheData = objectKeyValPairs(data).reduce(someETL, newEmptyDataThing());
+          const {
+            dateSeries,
+            rawCount,
+            avgDays7,
+            avgDays28
+          } = allTheData;
+          const dataForPlotly = [
+            {
+              type: "scatter",
+              mode: "markers",
+              x: dateSeries,
+              y: rawCount,
+            },
+            {
+              type: "scatter",
+              mode: "line",
+              x: dateSeries,
+              y: avgDays7,
+            },
+            {
+              type: "scatter",
+              mode: "line",
+              x: dateSeries,
+              y: avgDays28,
+            },
+          ];
           Plotly.newPlot("raw", dataForPlotly);
         })
       ; // fetch pipeline
@@ -207,7 +152,44 @@
   })();
 })(this);
 if (document.location.hash === "#show-chart") {
-  /* console.info("auto-showing chart"); */
   document.getElementsByClassName("chart-placeholder")[0].remove();
   this.showChart();
 }
+
+function objectValues(obj) {
+  return Object.keys(obj).map((key) => obj[key]);
+}
+
+function objectKeyValPairs(obj) {
+  return Object.keys(obj).map((key) => [key, obj[key]]);
+}
+
+function randomIntLessThan(max) {
+  return Math.floor(max * Math.random());
+}
+
+function flattener(a, b) {
+  return a.concat(b);
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  let error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+function padZero(string, length = 2) {
+  while (string.length < length) {
+    string = `0${string}`;
+  }
+  return string;
+}
+
+function sum(sum, n) {
+  return sum + n;
+}
+
+const keys = Object.keys.bind(Object);
