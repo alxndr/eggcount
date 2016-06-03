869e7af9783a1f6fc2e6066c9ed1600ae6a0a36a
Alexander <alxndr+github@gmail.com>
2016-06-03 10:19:50 -0700

    beginnings of a test suite

---
+++ test/utilities_spec.js
@@ -0,0 +1,22 @@
+import {expect} from "chai";
+import "mocha";
+
+import * as Utilities from "../src/utilities.js";
+
+describe("dayDifference", function() {
+  describe("with 1 day apart", function() {
+    it("returns 1", function() {
+      expect(Utilities.dayDifference("2016-06-01", "2016-06-02")).to.equal(1);
+    });
+  });
+  describe("1 year apart", function() {
+    it("returns 365", function() {
+      expect(Utilities.dayDifference("2013-06-01", "2014-06-01")).to.equal(365);
+    });
+    describe("on a leap year", function() {
+      it("returns 366", function() {
+        expect(Utilities.dayDifference("2016-01-01", "2017-01-01")).to.equal(366);
+      });
+    });
+  })
+});
