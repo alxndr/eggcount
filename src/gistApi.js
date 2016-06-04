a5e8ffa4dedb7fedf0f4b88d277900d57b240340
Alexander <alxndr+github@gmail.com>
2016-06-04 14:47:13 -0700

    get fancy new javascript working

--- src/gistApi.js
+++ src/gistApi.js
@@ -5,3 +5,16 @@ export function fetchGist(gistId) {
     .then(checkStatus)
     .then(extractJson);
 }
+
+export async function fetchFileInGist(filename, gistId) {
+  global.console.log(filename, gistId);
+  const {files, html_url} = await fetchGist(gistId);
+  const data =
+  await fetch(files[filename].raw_url)
+    .then(checkStatus)
+    .then(extractJson);
+  return {
+    fileUrl: html_url,
+    data: data
+  };
+}
