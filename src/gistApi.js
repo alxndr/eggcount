047c3de02d7f0bc70863d35bd90d86c211d409c4
Alexander <alxndr+github@gmail.com>
2016-06-06 23:58:00 -0700

    fetchJson helper

--- src/gistApi.js
+++ src/gistApi.js
@@ -1,17 +1,16 @@
 import {checkStatus, extractJson} from "./helpers/http";
 
+function fetchJson(url) {
+  return fetch(url).then(checkStatus).then(extractJson);
+}
+
 function fetchGist(gistId) {
-  return fetch(`https://api.github.com/gists/${gistId}`)
-    .then(checkStatus)
-    .then(extractJson);
+  return fetchJson(`https://api.github.com/gists/${gistId}`);
 }
 
 export async function fetchFileInGist(filename, gistId) {
   const {files, html_url} = await fetchGist(gistId);
-  const data =
-    await fetch(files[filename].raw_url)
-      .then(checkStatus)
-      .then(extractJson);
+  const data = await fetchJson(files[filename].raw_url);
   return {
     data,
     fileUrl: html_url
